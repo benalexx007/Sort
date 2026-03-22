2b037e103489b26beb30c780edee65af19f97b67
benalexx007 <ben.alexx.007@gmail.com>
2026-03-22 11:04:30 +0700

    add experience data

---
+++ Code/data_generation.cpp
@@ -0,0 +1,25 @@
+#include "library.h"
+
+using namespace std;
+
+int main() {
+    int sizes[] = {10000, 30000, 50000, 100000, 300000, 500000};
+    int orders[] = {0, 1, 2, 3};
+    string names[] = {"rand", "sorted", "rev", "nsorted"};
+    for (int size : sizes) {
+        for (int i = 0; i < 4; i++) {
+            int* a = new int[size];
+            GenerateData(a, size, orders[i]);
+            string filename = "../Experiment/" + names[i] + "/"  + to_string(size) + ".txt";
+            fstream fout(filename, ios::out);
+            fout << size << '\n';
+            for (int j = 0; j < size; j++) {
+                fout << a[j] << " ";
+            }
+            fout.close();
+            cout << "-> Da tao file: " << filename << '\n';
+            delete[] a;
+        }
+    }
+    return 0;
+}
